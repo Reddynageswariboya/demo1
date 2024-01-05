cc2d0006709771f87dddc7c5e1e37719cdf77721
Reddynageswariboya <155040660+Reddynageswariboya@users.noreply.github.com>
2024-01-05 12:03:12 +0530

    Add files via upload

---
+++ demo1.java
@@ -0,0 +1,27 @@
+class c1{
+public void reddy(){
+System.out.println("I am from AI&DS");
+}
+public void akhi(){
+System.out.println("I am from ME");
+}
+public void vaishu(){
+System.out.println("I am from CSE");
+}
+public void lakshmi(){
+System.out.println("I am from ECE");
+}
+public void sree(){
+System.out.println("I am from EEE");
+}
+}
+class c2{
+public static void main(String args[]){
+c1 ob=new c1();
+ob.reddy();
+ob.akhi();
+ob.vaishu();
+ob.lakshmi();
+ob.sree();
+}
+}
---
+++ progd1p02.java
@@ -0,0 +1,10 @@
+import java.util.scanner;
+class Demo{
+public static void main(String srgs[]){
+Scanner sc=new Scanner(System.in)
+int n=sc.nextInt();
+System.out.println("s table :");
+for(int i=n;i<50;i=i+5)
+system.out.println(i);
+}
+}
